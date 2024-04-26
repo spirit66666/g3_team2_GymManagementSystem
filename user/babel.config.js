8c79f50a1cbf0ee1ec9a926f158a399466ca8151
aries <c859633490@gmail.com>
2024-04-26 17:21:25 +0800

    1.2.0.1

--- user/babel.config.js
+++ user/babel.config.js
@@ -3,14 +3,5 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   plugins: ["@babel/plugin-transform-private-methods"],
-  proxyTable: {
-    '/api': {
-      target: 'http://localhost:9990',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': ''
-      }
-    }
-  },
 
 }
