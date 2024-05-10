d63378e0273e7ae5d318bfdd6bc390e9dfa28e20
aries <c859633490@gmail.com>
2024-05-10 13:06:30 +0800

    添加管理员端，可查看已存在注册用户信息

--- vite_gym/src/components/admin/manageuser.vue
+++ vite_gym/src/components/admin/manageuser.vue
@@ -1,148 +1,97 @@
 <template>
-  <el-row :gutter="16">
-    <el-col :span="8">
-      <div class="statistic-card">
-        <el-statistic :value="98500">
-          <template #title>
-            <div style="display: inline-flex; align-items: center">
-              Daily active users
-              <el-tooltip
-                  effect="dark"
-                  content="Number of users who logged into the product in one day"
-                  placement="top"
-              >
-                <el-icon style="margin-left: 4px" :size="12">
-                  <Warning />
-                </el-icon>
-              </el-tooltip>
-            </div>
-          </template>
-        </el-statistic>
-        <div class="statistic-footer">
-          <div class="footer-item">
-            <span>than yesterday</span>
-            <span class="green">
-              24%
-              <el-icon>
-                <CaretTop />
-              </el-icon>
-            </span>
-          </div>
-        </div>
-      </div>
-    </el-col>
-    <el-col :span="8">
-      <div class="statistic-card">
-        <el-statistic :value="693700">
-          <template #title>
-            <div style="display: inline-flex; align-items: center">
-              Monthly Active Users
-              <el-tooltip
-                  effect="dark"
-                  content="Number of users who logged into the product in one month"
-                  placement="top"
-              >
-                <el-icon style="margin-left: 4px" :size="12">
-                  <Warning />
-                </el-icon>
-              </el-tooltip>
-            </div>
-          </template>
-        </el-statistic>
-        <div class="statistic-footer">
-          <div class="footer-item">
-            <span>month on month</span>
-            <span class="red">
-              12%
-              <el-icon>
-                <CaretBottom />
-              </el-icon>
-            </span>
-          </div>
-        </div>
-      </div>
-    </el-col>
-    <el-col :span="8">
-      <div class="statistic-card">
-        <el-statistic :value="72000" title="New transactions today">
-          <template #title>
-            <div style="display: inline-flex; align-items: center">
-              New transactions today
-            </div>
-          </template>
-        </el-statistic>
-        <div class="statistic-footer">
-          <div class="footer-item">
-            <span>than yesterday</span>
-            <span class="green">
-              16%
-              <el-icon>
-                <CaretTop />
-              </el-icon>
-            </span>
-          </div>
-          <div class="footer-item">
-            <el-icon :size="14">
-              <ArrowRight />
-            </el-icon>
-          </div>
-        </div>
-      </div>
-    </el-col>
-  </el-row>
+
+
+    <el-config-provider >
+      <el-pagination
+      />
+      <el-table :data="tableData">
+        <el-table-column prop="id" label="ID"></el-table-column>
+        <el-table-column prop="username" label="用户名"></el-table-column>
+        <el-table-column prop="password" label="密码"></el-table-column>
+
+      </el-table>
+
+      <el-pagination  :total="total"
+                      :page-size="pageSize"
+                      :current-page="pageNumber"
+                      @current-change="handleCurrentChange"
+                      @size-change="handleSizeChange"
+                      :page-sizes="[1, 2, 3, 4]"
+
+
+                      layout="total, sizes, prev, pager, next, jumper"
+      />
+    </el-config-provider>
+
+
+
 </template>
 
-<script lang="ts" setup>
-import {
-  ArrowRight,
-  CaretBottom,
-  CaretTop,
-  Warning,
-} from '@element-plus/icons-vue'
+<script>
+
+export default {
+  data() {
+    return {
+      tableData: [],
+      pageNumber: 1,
+      pageSize: 2,
+      total: 0,
+      registerUsername: '',
+      registerPassword: '',
+      loginUsername: '',
+      loginPassword: ''
+    };
+  },
+
+  created() {
+    this.fetchData();
+
+  },
+  methods: {
+    fetchData() {
+      fetch("http://localhost:9990/page?pageNumber=" + this.pageNumber + "&pageSize="+ this.pageSize)
+          .then(response => response.json()).then(response => {
+
+        console.log(response);
+        this.total = response.total;
+
+        this.tableData = response.data;
+
+
+      });
+    },
+    registerUser() {
+      this.$http.post('/post?username='+this.registerUsername+'&password='+this.registerPassword).then(response => {
+
+        console.log(response);
+      })
+    },
+    loginUser() {
+      this.$http.put('/fetch').then(response => {
+        this.tableData = response.data;
+
+        console.log(this.tableData);
+        if (response.data.find(user => user.username === this.loginUsername && user.password === this.loginPassword)) {
+
+          console.log('登陆成功');
+          this.$router.push('/home');
+          // 登陆成功处理逻辑
+        } else {
+          console.log('登陆失败');
+          // 登陆失败处理逻辑
+        }
+      })
+    },
+    handleCurrentChange(currentPage) {
+      console.log(currentPage);
+      this.pageNumber = currentPage;
+      this.fetchData();
+    },
+    handleSizeChange(pageSize) {
+      console.log(pageSize);
+      this.pageSize = pageSize;
+      this.fetchData();}
+
+  }
+};
 </script>
-
-<style scoped>
-:global(h2#card-usage ~ .example .example-showcase) {
-  background-color: var(--el-fill-color) !important;
-}
-
-.el-statistic {
-  --el-statistic-content-font-size: 28px;
-}
-
-.statistic-card {
-  height: 100%;
-  padding: 20px;
-  border-radius: 4px;
-  background-color: var(--el-bg-color-overlay);
-}
-
-.statistic-footer {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  font-size: 12px;
-  color: var(--el-text-color-regular);
-  margin-top: 16px;
-}
-
-.statistic-footer .footer-item {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.statistic-footer .footer-item span:last-child {
-  display: inline-flex;
-  align-items: center;
-  margin-left: 4px;
-}
-
-.green {
-  color: var(--el-color-success);
-}
-.red {
-  color: var(--el-color-error);
-}
-</style>
