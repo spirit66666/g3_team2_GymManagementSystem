500995e76c503022680f390fecfd427a0ee790c3
aries <c859633490@gmail.com>
2024-05-14 21:09:49 +0800

    增加后端usercontrol

--- servet/src/main/java/org/gym/servet/ServetApplication.java
+++ servet/src/main/java/org/gym/servet/ServetApplication.java
@@ -19,45 +19,6 @@ import java.util.Map;
 @CrossOrigin
 public class ServetApplication {
 
-    @Autowired
-    getuser getuser;
-
-
-
-    @GetMapping("/page")
-    public Map<String, Object>  findPage(@RequestParam Integer pageNumber, @RequestParam Integer pageSize) {
-        pageNumber = (pageNumber - 1)*pageSize;
-        Integer total = getuser.count();
-        List<User> data = getuser.selectPage(pageNumber, pageSize);
-        Map<String, Object> map = new HashMap<>();
-        map.put("data", data);
-        map.put("total", total);
-
-        return map;
-
-
-    }
-
-    @GetMapping("/us")
-    public List<User> getUsers1() {
-
-        List<User> users = getuser.find();
-        return users ;
-
-    }
-
-    @PutMapping("/fetch")
-    public List<User> getUsers12() {
-
-        return getuser.find();
-    }
-
-    @PostMapping("/post")
-    public User addUser(User user) {
-        getuser.insertUser(user);
-        return user;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ServetApplication.class, args);
     }
---
+++ servet/src/main/java/org/gym/servet/controller/usercontrol.java
@@ -0,0 +1,60 @@
+package org.gym.servet.controller;
+
+import org.gym.servet.entity.User;
+import org.gym.servet.getmapper.getuser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@ResponseBody
+@SpringBootApplication
+@RestController
+//allow frontend to access backend
+@CrossOrigin
+public class usercontrol {
+
+    @Autowired
+    org.gym.servet.getmapper.getuser getuser;
+
+
+
+    @GetMapping("/page")
+    public Map<String, Object> findPage(@RequestParam Integer pageNumber, @RequestParam Integer pageSize) {
+        pageNumber = (pageNumber - 1)*pageSize;
+        Integer total = getuser.count();
+        List<User> data = getuser.selectPage(pageNumber, pageSize);
+        Map<String, Object> map = new HashMap<>();
+        map.put("data", data);
+        map.put("total", total);
+
+        return map;
+
+
+    }
+
+    @GetMapping("/us")
+    public List<User> getUsers1() {
+
+        List<User> users = getuser.find();
+        return users ;
+
+    }
+
+    @PutMapping("/fetch")
+    public List<User> getUsers12() {
+
+        return getuser.find();
+    }
+
+    @PostMapping("/post")
+    public User addUser(User user) {
+        getuser.insertUser(user);
+        return user;
+    }
+
+}
