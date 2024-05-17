41e210db1d8458b59cc29e01c93ed06dd209e37b
aries <c859633490@gmail.com>
2024-05-17 17:17:11 +0800

    修改部分功能

--- vite_gym/src/components/differ_page/second_page.vue
+++ vite_gym/src/components/differ_page/second_page.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
 
+    <input type="text" v-model="inputText"> </input>
+<el-button type="primary" @click="gps_callback">Get GP</el-button>
+    <div>
+      <p>{{ output }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'SecondPage'
+  name: 'SecondPage',
+  data() {
+    return {
+      // data
+      output: '',
+      inputText: ''
+    }
+  },
+  methods: {
+    // methods
+    gps_callback() {
+      console.log("11");
+      axios.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyAx7AeiyZ-o0wEm2EKFT24_AJpsRO_hLSM',
+          {"contents":[{"parts":[{"text":this.inputText}]}]}).then(response => {
+        console.log(response.data.candidates[0].content.parts[0].text);
+        this.output = response.data.candidates[0].content.parts[0].text;
+      })
+    }
+  }
 }
 </script>
 
